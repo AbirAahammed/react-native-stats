c961eb0e864eb058e1d2e17218cb1069d85f19af
abirAsakib <ahammed.abir47@gmail.com>
2020-04-30 03:41:30 -0500

    Primary changes

--- App.js
+++ App.js
@@ -1,51 +1,11 @@
 import * as React from 'react';
 import { Appbar, Avatar, Button, Card, Checkbox, DataTable, FAB  } from 'react-native-paper';
 import { StyleSheet, View } from 'react-native';
-
+import Feed from './feed'
 export default class MyComponent extends React.Component {
   render() {
     return (
-
-      <View style={styles.view}>
-        <Appbar style={styles.appbar}>
-          <Appbar.Action style={styles.menu} icon="archive" onPress={() => console.log('Pressed archive')} />
-          <Appbar.Action style={styles.menu} icon="label" onPress={() => console.log('Pressed label')} />
-          <Appbar.Action style={styles.menu} icon="delete" onPress={() => console.log('Pressed delete')} />
-          <Appbar.Action style={styles.menu} icon="star" onPress={() => console.log('Pressed delete')} />
-        </Appbar>
-        <FAB
-    style={styles.fab}
-    small
-    icon="archive"
-    onPress={() => console.log('Pressed')}
-  />
-      </View>
+      <Feed/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-
-  view: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0
-  },
-  appbar: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0,
-    height: 40
-  },
-  fab: {
-    position: 'absolute',
-    margin: 16,
-    right: 0,
-    bottom: 20,
-  },
-  menu: {
-    width: 90
-  }
-});
---
+++ feed.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text, Button, TouchableOpacity } from 'react-native';
+
+import { styles } from './styles/styles';
+import { color } from 'react-native-reanimated';
+
+class Feed extends Component {
+    render () {
+        return (
+            <View style={styles.center}>
+                <Text style={styles.title}>Navigation Drawer</Text>
+                <TouchableOpacity>
+        <Text style={styles.underLineText}>Your underline Text</Text>
+      </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+export default Feed;
